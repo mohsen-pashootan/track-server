977ebca0b87406ba2e525f3070a3ad999fb2ea3c
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2021-11-24 21:13:09 +0330

    connect to mongo db data base with mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://mohsen-admin:mohsen-password@track-server-cluster.osiil.mongodb.net/track-server-cluster?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  //   useCreateIndex: true, // Deprecated
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log("Error connecting to mongo", error);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi There");
