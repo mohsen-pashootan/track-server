5e9bc113b8a2cea3e6eebe2079e3efc63f665210
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2022-05-10 23:43:14 +0430

    add cors policy config

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
+const cors = require("cors"); // for locally test not sure for Pro version
 
 const app = express();
+app.options(
+  "*",
+  cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }) // for locally test not sure for Pro version
+);
 
 app.use(express.json());
 app.use(
@@ -16,7 +21,7 @@ app.use(
 );
 app.use(authRoutes);
 app.use(trackRoutes);
-
+app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 })); // for locally test not sure for Pro version
 const mongoUri =
   "mongodb+srv://mohsen-admin:mohsen-password@track-server-cluster.osiil.mongodb.net/track-server-cluster?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
